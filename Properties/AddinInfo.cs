b687172ced3d94cc319e69fdbeffa831660effd3
Michael Hutchinson <m.j.hutchinson@gmail.com>
2015-03-16 23:53:03 -0400

    Initial commit

---
+++ Properties/AddinInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+	"MonoDevelop.MSBuildEditor", 
+	Namespace = "MonoDevelop.MSBuildEditor",
+	Version = "1.0"
+)]
+
+[assembly:AddinName ("MonoDevelop.MSBuildEditor")]
+[assembly:AddinCategory ("MonoDevelop.MSBuildEditor")]
+[assembly:AddinDescription ("MonoDevelop.MSBuildEditor")]
+[assembly:AddinAuthor ("Michael Hutchinson")]
