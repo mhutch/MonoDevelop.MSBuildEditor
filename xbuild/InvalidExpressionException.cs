138ec16032a0a5034f3edc0f0c973d713bbc9450
mhutch <m.j.hutchinson@gmail.com>
2015-11-06 18:27:32 -0500

    Import xbuild's expression parser

---
+++ xbuild/InvalidExpressionException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Microsoft.Build.BuildEngine
+{
+	[Serializable]
+	class InvalidExpressionException : Exception
+	{
+		public InvalidExpressionException ()
+		{
+		}
+
+		public InvalidExpressionException (string message) : base (message)
+		{
+		}
+
+		public InvalidExpressionException (string message, Exception innerException) : base (message, innerException)
+		{
+		}
+
+		protected InvalidExpressionException (SerializationInfo info, StreamingContext context) : base (info, context)
+		{
+		}
+	}
+}
