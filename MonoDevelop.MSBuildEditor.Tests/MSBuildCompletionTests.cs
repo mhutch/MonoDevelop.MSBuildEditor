97492edb196c03acb3190eb8b15241e68520ce26
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2017-11-03 18:31:15 -0400

    Add some more completion tests

--- MonoDevelop.MSBuildEditor.Tests/MSBuildCompletionTests.cs
+++ MonoDevelop.MSBuildEditor.Tests/MSBuildCompletionTests.cs
@@ -18,6 +18,28 @@ namespace MonoDevelop.MSBuildEditor.Tests
 		}
 
 		[Test]
+		public async Task InferredItems ()
+		{
+			var provider = await MSBuildEditorTesting.CreateProvider (@"
+<Project><ItemGroup><Foo /><Bar /><$", ".csproj");
+			Assert.IsNotNull (provider);
+			Assert.IsNotNull (provider.Find ("Foo"));
+			Assert.IsNotNull (provider.Find ("Bar"));
+			// comment, cdata, closing tags for Project and ItemGroup, plus the actual two items
+			Assert.AreEqual (6, provider.Count);
+		}
+
+		[Test]
+		public async Task InferredMetadata ()
+		{
+			var provider = await MSBuildEditorTesting.CreateProvider (@"
+<Project><ItemGroup><Foo><Bar>a</Bar></Foo><Foo><$", ".csproj");
+			Assert.IsNotNull (provider);
+			Assert.IsNotNull (provider.Find ("Bar"));
+			Assert.AreEqual (6, provider.Count);
+		}
+
+		[Test]
 		public async Task InferredMetadataAttribute ()
 		{
 			var provider = await MSBuildEditorTesting.CreateProvider (@"
