bc13f15369ffc89d2e6094b41f6d516ceac98376
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2018-02-16 01:22:19 -0500

    Bump version to 2.0.2

--- MonoDevelop.MSBuildEditor/Properties/AddinInfo.cs
+++ MonoDevelop.MSBuildEditor/Properties/AddinInfo.cs
@@ -4,7 +4,7 @@ using Mono.Addins.Description;
 [assembly: Addin (
 	"MonoDevelop.MSBuildEditor",
 	Namespace = "MonoDevelop.MSBuildEditor",
-	Version = "2.0.1"
+	Version = "2.0.2"
 )]
 
 [assembly: AddinName ("MSBuild Editor")]
