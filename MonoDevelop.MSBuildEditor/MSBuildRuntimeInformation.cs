b92d7b6ed8d48c9016246b9278629ebff2eece6f
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2018-03-28 20:22:56 -0400

    Cache resolved SDK paths

--- MonoDevelop.MSBuildEditor/MSBuildRuntimeInformation.cs
+++ MonoDevelop.MSBuildEditor/MSBuildRuntimeInformation.cs
@@ -1,7 +1,10 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Collections.Generic;
+
+using Microsoft.Build.Framework;
 using MonoDevelop.Core;
 using MonoDevelop.Core.Assemblies;
 using MonoDevelop.MSBuildEditor.Language;
@@ -13,6 +16,7 @@ namespace MonoDevelop.MSBuildEditor
 		TargetRuntime target;
 		MSBuildSdkResolver sdkResolver;
 		string tvString;
+		Dictionary<SdkReference, string> resolvedSdks = new Dictionary<SdkReference, string> (); 
 
 		public MSBuildRuntimeInformation (TargetRuntime target, MSBuildToolsVersion tv)
 		{
@@ -37,7 +41,17 @@ namespace MonoDevelop.MSBuildEditor
 
 		public List<MSBuildSdkResolver.SdkInfo> GetRegisteredSdks () => sdkResolver.GetRegisteredSdks ();
 
-		public string GetSdkPath (Microsoft.Build.Framework.SdkReference sdk, string projectFile, string solutionPath)
-			=> sdkResolver.GetSdkPath (sdk, projectFile, solutionPath);
+		public string GetSdkPath (SdkReference sdk, string projectFile, string solutionPath)
+		{
+			if (!resolvedSdks.TryGetValue(sdk, out string path)) {
+				try {
+					path = sdkResolver.GetSdkPath (sdk, projectFile, solutionPath);
+				} catch (Exception ex) {
+					LoggingService.LogError ("Error in SDK resolver", ex);
+				}
+				resolvedSdks [sdk] = path;
+			}
+			return path;
+		}
 	}
 }
