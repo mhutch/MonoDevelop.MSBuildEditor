ca8385b59995151044020e63f60d35f279792b3e
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2019-04-19 13:53:06 +0100

    Update some property metadata

--- MonoDevelop.MSBuildEditor/Schema/Builtins.cs
+++ MonoDevelop.MSBuildEditor/Schema/Builtins.cs
@@ -25,12 +25,12 @@ namespace MonoDevelop.MSBuildEditor.Schema
 		static Builtins ()
 		{
 			AddMetadata ("FullPath", "The full path of the item", MSBuildValueKind.File);
-			AddMetadata ("RootDir", "The root directory of the item", MSBuildValueKind.Folder);
+			AddMetadata ("RootDir", "The root directory of the item", MSBuildValueKind.FolderWithSlash);
 			AddMetadata ("Filename", "The filename of the item", MSBuildValueKind.Filename);
 			AddMetadata ("Extension", "The file extension of the item", MSBuildValueKind.Extension);
-			AddMetadata ("RelativeDir", "The path specified in the Include attribute", MSBuildValueKind.Folder);
-			AddMetadata ("Directory", "The directory of the item, excluding the root directory", MSBuildValueKind.Folder);
-			AddMetadata ("RecursiveDir", "If the item contained a ** wildstar, the value to which it was expanded", MSBuildValueKind.Folder);
+			AddMetadata ("RelativeDir", "The path specified in the Include attribute", MSBuildValueKind.FolderWithSlash);
+			AddMetadata ("Directory", "The directory of the item, excluding the root directory", MSBuildValueKind.FolderWithSlash);
+			AddMetadata ("RecursiveDir", "If the item contained a ** wildstar, the value to which it was expanded", MSBuildValueKind.FolderWithSlash);
 			AddMetadata ("Identity", "The value specified in the Include attribute", MSBuildValueKind.MatchItem);
 			AddMetadata ("ModifiedTime", "The time the the item was last modified", MSBuildValueKind.DateTime);
 			AddMetadata ("CreatedTime", "The time the the item was created", MSBuildValueKind.DateTime);
@@ -56,21 +56,20 @@ namespace MonoDevelop.MSBuildEditor.Schema
 			AddProperty ("MSBuildProjectName", "Name of the project file, excluding extension.", MSBuildValueKind.Filename);
 			AddProperty ("MSBuildStartupDirectory", "Absolute path of the directory where MSBuild is invoked. Does not include final backslash.", MSBuildValueKind.Folder);
 			AddProperty ("MSBuildThisFile", "Name of the current MSBuild file, including extension.", MSBuildValueKind.Filename);
-			AddProperty ("MSBuildThisFileDirectory", "Directory where the current MSBuild file is located. Includes final backslash.", MSBuildValueKind.Folder);
-			AddProperty ("MSBuildThisFileDirectoryNoRoot", "Directory where the current MSBuild file is located, excluding drive root. Includes final backslash.", MSBuildValueKind.Folder);
+			AddProperty ("MSBuildThisFileDirectory", "Directory where the current MSBuild file is located.", MSBuildValueKind.FolderWithSlash);
+			AddProperty ("MSBuildThisFileDirectoryNoRoot", "Directory where the current MSBuild file is located, excluding drive root.", MSBuildValueKind.FolderWithSlash);
 			AddProperty ("MSBuildThisFileExtension", "Extension of the current MSBuild file.", MSBuildValueKind.Extension);
 			AddProperty ("MSBuildThisFileFullPath", "Absolute path of the current MSBuild file.", MSBuildValueKind.File);
 			AddProperty ("MSBuildThisFileName", "Name of the current MSBuild file, excluding extension.", MSBuildValueKind.File);
 			AddProperty ("MSBuildToolsPath", "Path to the current toolset, specfied by the MSBuildToolsVersion. Does not include final backslash.", MSBuildValueKind.Folder);
 			AddProperty ("MSBuildToolsVersion", "Version of the current toolset", MSBuildValueKind.ToolsVersion);
-			AddProperty ("MSBuildUserExtensionsPath", "Directory from which user extensions are imported", MSBuildValueKind.Folder);
-			AddProperty ("LocalAppData", "The local application data directory", MSBuildValueKind.Folder);
+			AddProperty ("MSBuildUserExtensionsPath", "Directory from which user extensions are imported. Does not include final backslash.", MSBuildValueKind.Folder);
 			AddProperty ("MSBuildRuntimeType", "The runtime on which MSBuild is running", MSBuildValueKind.HostRuntime);
 			AddProperty ("MSBuildOverrideTasksPath", "Path to files that override built-in tasks", MSBuildValueKind.Folder);
 			AddProperty ("DefaultOverrideToolsVersion", "Tools version to override the built-in tools version", MSBuildValueKind.ToolsVersion);
 			AddProperty ("MSBuildAssemblyVersion", "The version of the MSBuild assemblies", MSBuildValueKind.Version);
 			AddProperty ("OS", "The OS on which MSBuild is running", MSBuildValueKind.HostOS);
-			AddProperty ("MSBuildFrameworkToolsRoot", "The root directory of the .NET framework tools", MSBuildValueKind.Folder);
+			AddProperty ("MSBuildFrameworkToolsRoot", "The root directory of the .NET framework tools", MSBuildValueKind.FolderWithSlash);
 
 			AddProperty ("MSBuildTreatWarningsAsErrors", "Name of the property that indicates that all warnings should be treated as errors", MSBuildValueKind.PropertyName, true);
 			AddProperty ("MSBuildWarningsAsErrors", "Name of the property that indicates a list of warnings to treat as errors", MSBuildValueKind.PropertyName, true);
