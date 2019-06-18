e90123173cb6b5c9f2b7e09e12afe9abd21d29b3
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2019-06-18 15:07:15 -0400

    Port WIP analyzer logic

---
+++ MonoDevelop.MSBuild/Analysis/MSBuildDiagnosticSeverity.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+
+namespace MonoDevelop.MSBuild.Analysis
+{
+	public enum MSBuildDiagnosticSeverity
+	{
+		None,
+		Info,
+		Warning,
+		Error
+	}
+}
---
+++ MonoDevelop.MSBuild/Analysis/MSBuildRefactoringProvider.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Threading.Tasks;
+
+namespace MonoDevelop.MSBuild.Analysis
+{
+	abstract class MSBuildRefactoringProvider
+	{
+		public abstract Task<MSBuildAction> GetRefactoringsAsync (MSBuildRefactoringContext context);
+	}
+}
