6cfad5593f9a443ff4c85e282ef7fb2f24cefcbc
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2019-06-18 20:12:50 -0400

    Add simple QuickInfo test
    Not working due to limitations in MiniEditor

--- MonoDevelop.MSBuild.Tests/MSBuildQuickInfoTests.cs
+++ MonoDevelop.MSBuild.Tests/MSBuildQuickInfoTests.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.MiniEditor;
 using Microsoft.VisualStudio.Utilities;
@@ -12,18 +13,16 @@ using NUnit.Framework;
 namespace MonoDevelop.MSBuild.Tests
 {
 	[TestFixture]
-	public class MSBuildCompletionTests : CompletionTestBase
+	[Ignore ("There is currently no quickinfo broker")]
+	public class MSBuildQuickInfoTests : CompletionTestBase
 	{
 		public override IContentType ContentType => Catalog.ContentTypeRegistryService.GetContentType (MSBuildContentType.Name);
 
 		[Test]
-		public async Task TestElementCompletion ()
+		public async Task TestItemGroupQuickInfo ()
 		{
-			var result = await GetCompletionContext ("<Project>$");
-			Assert.IsTrue (result.Items.Length > 0);
-			result.AssertContains ("ItemGroup");
-			result.AssertContains ("Choose");
-			result.AssertContains ("Import");
+			var result = await GetQuickInfoItems ("<Project><Item$Group>");
+			Assert.IsTrue (result.Items.Any ());
 		}
 
 		protected override (EditorEnvironment, EditorCatalog) InitializeEnvironment () => TestEnvironment.EnsureInitialized ();
