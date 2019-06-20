3ec933b662337045233ed1b20a03fe1895a80579
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2019-06-20 15:57:28 -0400

    Harden parser against exceptions

--- MonoDevelop.MSBuild.Editor/MSBuildBackgroundParser.cs
+++ MonoDevelop.MSBuild.Editor/MSBuildBackgroundParser.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Build.Evaluation;
@@ -13,7 +14,17 @@ namespace MonoDevelop.MSBuild.Editor.Completion
 {
 	class MSBuildBackgroundParser : XmlBackgroundParser<MSBuildParseResult>
 	{
-		readonly IRuntimeInformation runtimeInformation = new ProjectCollectionRuntimeInformation (ProjectCollection.GlobalProjectCollection);
+		readonly IRuntimeInformation runtimeInformation;
+
+		public MSBuildBackgroundParser ()
+		{
+			try {
+				runtimeInformation = new ProjectCollectionRuntimeInformation (ProjectCollection.GlobalProjectCollection);
+			} catch (Exception ex) {
+				LoggingService.LogError ("Failed to initialize runtime info for parser", ex);
+				runtimeInformation = new NullRuntimeInformation ();
+			}
+		}
 
 		protected override Task<MSBuildParseResult> StartParseAsync (
 			ITextSnapshot2 snapshot, MSBuildParseResult previousParse,
@@ -26,7 +37,13 @@ namespace MonoDevelop.MSBuild.Editor.Completion
 				string filename = "foo.csproj";
 				var schemaProvider = new MSBuildSchemaProvider ();
 
-				var doc = MSBuildRootDocument.Parse (filename, snapshot.GetTextSource (), oldDoc, schemaProvider, runtimeInformation, token);
+				MSBuildRootDocument doc = MSBuildRootDocument.Empty;
+				try {
+					doc = MSBuildRootDocument.Parse (filename, snapshot.GetTextSource (), oldDoc, schemaProvider, runtimeInformation, token);
+				} catch (Exception ex) {
+					LoggingService.LogError ("Unhandled error in MSBuild parser", ex);
+					doc = MSBuildRootDocument.Empty;
+				}
 
 				return new MSBuildParseResult (doc, doc.XDocument, doc.Errors);
 			});
