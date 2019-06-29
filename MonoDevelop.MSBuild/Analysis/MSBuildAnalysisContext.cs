e4f35c2b2b61daa417a2f649fe2663cc20c495bd
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2019-06-29 01:41:21 -0400

    Make MSBuildAnalysisContext more closely match the visitor

--- MonoDevelop.MSBuild/Analysis/MSBuildAnalysisContext.cs
+++ MonoDevelop.MSBuild/Analysis/MSBuildAnalysisContext.cs
@@ -1,32 +1,41 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
 using System.Collections.Immutable;
 using System.Threading;
 using MonoDevelop.MSBuild.Language;
+using MonoDevelop.MSBuild.Language.Expressions;
 using MonoDevelop.MSBuild.Schema;
+using MonoDevelop.Xml.Dom;
 
 namespace MonoDevelop.MSBuild.Analysis
 {
 	abstract class MSBuildAnalysisContext
 	{
-		public abstract void RegisterElementAction(Action<ElementDiagnosticContext> action, ImmutableArray<MSBuildKind> elementKinds);
-		public abstract void RegisterItemAction(Action<ItemDiagnosticContext> action, ImmutableArray<string> itemNames);
-		public abstract void RegisterPropertyAction(Action<PropertyDiagnosticContext> action, ImmutableArray<string> propertyNames);
+		public abstract void RegisterResolvedElementAction(Action<ResolvedElementDiagnosticContext> action, ImmutableArray<MSBuildKind> elementKinds);
+		public abstract void RegisterResolvedAttributeAction (Action<ResolvedAttributeDiagnosticContext> action, ImmutableArray<(MSBuildKind elementKind, string attributeName)> attributeNames);
+		public abstract void RegisterUnknownElementAction (Action<UnknownElementDiagnosticContext> action);
+		public abstract void RegisterUnknownAttributeAction (Action<UnknownAttributeDiagnosticContext> action);
+		public abstract void RegisterResolvedElementValueAction (Action<ElementValueDiagnosticContext> action, ImmutableArray<MSBuildKind> elementKinds);
+		public abstract void RegisterResolvedAttributeValueAction (Action<AttributeValueDiagnosticContext> action, ImmutableArray<(MSBuildKind elementKind, string attributeName)> attributeNames);
+		public abstract void RegisterResolvedTypedValueAction (Action<TypedValueDiagnosticContext> action, ImmutableArray<MSBuildValueKind> valueKind);
+		public abstract void RegisterExpressionNodeAction (Action<ExpressionNodeDiagnosticContext> action, ImmutableArray<ExpressionNodeKind> nodeKind);
 	}
 
-	struct ElementDiagnosticContext
+	struct ResolvedElementDiagnosticContext
 	{
 		readonly Action<MSBuildDiagnostic> reportDiagnostic;
 		public MSBuildDocument Document { get; }
 		public CancellationToken CancellationToken { get; }
 		public MSBuildLanguageElement Element { get; }
+		public XElement XElement { get; }
 
-		public ElementDiagnosticContext (MSBuildDocument document, MSBuildLanguageElement element, Action<MSBuildDiagnostic> reportDiagnostic, CancellationToken cancellationToken)
+		public ResolvedElementDiagnosticContext (MSBuildLanguageElement element, XElement xelement, MSBuildDocument document, Action<MSBuildDiagnostic> reportDiagnostic, CancellationToken cancellationToken)
 		{
 			Document = document;
 			Element = element;
+			XElement = xelement;
 			this.reportDiagnostic = reportDiagnostic;
 			CancellationToken = cancellationToken;
 		}
@@ -37,13 +46,13 @@ namespace MonoDevelop.MSBuild.Analysis
 		}
 	}
 
-	struct ItemDiagnosticContext
+	struct ResolvedAttributeDiagnosticContext
 	{
 		readonly Action<MSBuildDiagnostic> reportDiagnostic;
 		public MSBuildDocument Document { get; }
 		public CancellationToken CancellationToken { get; }
 
-		public ItemDiagnosticContext(MSBuildDocument document, Action<MSBuildDiagnostic> reportDiagnostic, CancellationToken cancellationToken)
+		public ResolvedAttributeDiagnosticContext (MSBuildDocument document, Action<MSBuildDiagnostic> reportDiagnostic, CancellationToken cancellationToken)
 		{
 			Document = document;
 			this.reportDiagnostic = reportDiagnostic;
@@ -56,13 +65,108 @@ namespace MonoDevelop.MSBuild.Analysis
 		}
 	}
 
-	struct PropertyDiagnosticContext
+	struct UnknownElementDiagnosticContext
 	{
 		readonly Action<MSBuildDiagnostic> reportDiagnostic;
 		public MSBuildDocument Document { get; }
 		public CancellationToken CancellationToken { get; }
 
-		public PropertyDiagnosticContext (MSBuildDocument document, Action<MSBuildDiagnostic> reportDiagnostic, CancellationToken cancellationToken)
+		public UnknownElementDiagnosticContext (MSBuildDocument document, Action<MSBuildDiagnostic> reportDiagnostic, CancellationToken cancellationToken)
+		{
+			Document = document;
+			this.reportDiagnostic = reportDiagnostic;
+			CancellationToken = cancellationToken;
+		}
+
+		public void ReportDiagnostic (MSBuildDiagnostic diagnostic)
+		{
+			reportDiagnostic (diagnostic);
+		}
+	}
+
+	struct UnknownAttributeDiagnosticContext
+	{
+		readonly Action<MSBuildDiagnostic> reportDiagnostic;
+		public MSBuildDocument Document { get; }
+		public CancellationToken CancellationToken { get; }
+
+		public UnknownAttributeDiagnosticContext (MSBuildDocument document, Action<MSBuildDiagnostic> reportDiagnostic, CancellationToken cancellationToken)
+		{
+			Document = document;
+			this.reportDiagnostic = reportDiagnostic;
+			CancellationToken = cancellationToken;
+		}
+
+		public void ReportDiagnostic (MSBuildDiagnostic diagnostic)
+		{
+			reportDiagnostic (diagnostic);
+		}
+	}
+
+	struct ElementValueDiagnosticContext
+	{
+		readonly Action<MSBuildDiagnostic> reportDiagnostic;
+		public MSBuildDocument Document { get; }
+		public CancellationToken CancellationToken { get; }
+
+		public ElementValueDiagnosticContext (MSBuildDocument document, Action<MSBuildDiagnostic> reportDiagnostic, CancellationToken cancellationToken)
+		{
+			Document = document;
+			this.reportDiagnostic = reportDiagnostic;
+			CancellationToken = cancellationToken;
+		}
+
+		public void ReportDiagnostic (MSBuildDiagnostic diagnostic)
+		{
+			reportDiagnostic (diagnostic);
+		}
+	}
+
+	struct AttributeValueDiagnosticContext
+	{
+		readonly Action<MSBuildDiagnostic> reportDiagnostic;
+		public MSBuildDocument Document { get; }
+		public CancellationToken CancellationToken { get; }
+
+		public AttributeValueDiagnosticContext (MSBuildDocument document, Action<MSBuildDiagnostic> reportDiagnostic, CancellationToken cancellationToken)
+		{
+			Document = document;
+			this.reportDiagnostic = reportDiagnostic;
+			CancellationToken = cancellationToken;
+		}
+
+		public void ReportDiagnostic (MSBuildDiagnostic diagnostic)
+		{
+			reportDiagnostic (diagnostic);
+		}
+	}
+
+	struct TypedValueDiagnosticContext
+	{
+		readonly Action<MSBuildDiagnostic> reportDiagnostic;
+		public MSBuildDocument Document { get; }
+		public CancellationToken CancellationToken { get; }
+
+		public TypedValueDiagnosticContext (MSBuildDocument document, Action<MSBuildDiagnostic> reportDiagnostic, CancellationToken cancellationToken)
+		{
+			Document = document;
+			this.reportDiagnostic = reportDiagnostic;
+			CancellationToken = cancellationToken;
+		}
+
+		public void ReportDiagnostic (MSBuildDiagnostic diagnostic)
+		{
+			reportDiagnostic (diagnostic);
+		}
+	}
+
+	struct ExpressionNodeDiagnosticContext
+	{
+		readonly Action<MSBuildDiagnostic> reportDiagnostic;
+		public MSBuildDocument Document { get; }
+		public CancellationToken CancellationToken { get; }
+
+		public ExpressionNodeDiagnosticContext (MSBuildDocument document, Action<MSBuildDiagnostic> reportDiagnostic, CancellationToken cancellationToken)
 		{
 			Document = document;
 			this.reportDiagnostic = reportDiagnostic;
