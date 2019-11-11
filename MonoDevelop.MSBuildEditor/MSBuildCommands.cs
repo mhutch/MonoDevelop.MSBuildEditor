1cd7038c97b9a2f550eb1cc605f44c643c8e5bf4
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2019-11-11 16:51:45 +0100

    Delete rest of the old editor impl

--- MonoDevelop.MSBuildEditor/MSBuildCommands.cs
+++ MonoDevelop.MSBuildEditor/MSBuildCommands.cs
@@ -11,41 +11,7 @@ namespace MonoDevelop.MSBuildEditor
 {
 	public enum MSBuildCommands
 	{
-		NavigationOperations,
 		ToggleShowPrivateSymbols
-	}
-
-	sealed class MSBuildNavigationOperationsCommandHandler : CommandHandler
-	{
-		protected override void Run (object dataItem)
-		{
-			((Action)dataItem)();
-		}
-
-		protected override void Update (CommandArrayInfo info)
-		{
-			/*
-			var doc = IdeApp.Workbench.ActiveDocument;
-			if (doc == null || doc.FileName == FilePath.Null || doc.DocumentContext.ParsedDocument == null)
-				return;
-
-			var msbuildEditor = doc.GetContent<MSBuildTextEditorExtension> ();
-			if (msbuildEditor == null) {
-				return;
-			}
-
-			CommandInfo goToDeclarationCommand = IdeApp.CommandService.GetCommandInfo (RefactoryCommands.GotoDeclaration);
-			CommandInfo findReferenceCommand = IdeApp.CommandService.GetCommandInfo (RefactoryCommands.FindReferences);
-
-			if (goToDeclarationCommand.Enabled) {
-				info.Add (goToDeclarationCommand, new Action (() => IdeApp.CommandService.DispatchCommand (RefactoryCommands.GotoDeclaration)));
-			}
-
-			if (findReferenceCommand.Enabled) {
-				info.Add (findReferenceCommand, new Action (() => IdeApp.CommandService.DispatchCommand (RefactoryCommands.FindReferences)));
-			}
-			*/
-		}
 	}
 
 	sealed class ToggleShowPrivateSymbolsHandler : CommandHandler
