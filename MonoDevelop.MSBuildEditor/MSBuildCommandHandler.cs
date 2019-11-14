b7b35fec2c0effdf2a471b801116ef6e75e94701
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2019-11-14 12:34:32 +0100

    Port SwitchBetweenRelatedFiles command

---
+++ MonoDevelop.MSBuildEditor/MSBuildCommandHandler.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.ComponentModel.Composition;
+using System.IO;
+
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide;
+using MonoDevelop.MSBuild.Editor;
+using MonoDevelop.TextEditor;
+
+namespace MonoDevelop.MSBuildEditor
+{
+	[Export (typeof (IEditorContentProvider))]
+	[ContentType (MSBuildContentType.Name)]
+	[TextViewRole (PredefinedTextViewRoles.PrimaryDocument)]
+	sealed class MSBuildCommandHandlerContentProvider : EditorContentInstanceProvider<MSBuildCommandHandler>
+	{
+		[Import]
+		public ITextDocumentFactoryService TextDocumentFactory { get; private set; }
+
+		protected override MSBuildCommandHandler CreateInstance (ITextView view)
+						=> new MSBuildCommandHandler (view, this);
+	}
+
+	class MSBuildCommandHandler
+	{
+		readonly ITextView view;
+		readonly MSBuildCommandHandlerContentProvider provider;
+
+		public MSBuildCommandHandler (ITextView view, MSBuildCommandHandlerContentProvider provider)
+		{
+			this.view = view;
+			this.provider = provider;
+		}
+
+		string TryGetFilename () =>
+						provider.TextDocumentFactory.TryGetTextDocument (view.TextBuffer, out var doc)
+										? doc.FilePath
+										: null;
+
+		[CommandHandler (DesignerSupport.Commands.SwitchBetweenRelatedFiles)]
+		protected void Run ()
+		{
+			var counterpart = GetCounterpartFile ();
+			IdeApp.Workbench.OpenDocument (counterpart, null, true);
+		}
+
+		[CommandUpdateHandler (DesignerSupport.Commands.SwitchBetweenRelatedFiles)]
+		protected void Update (CommandInfo info)
+		{
+			info.Enabled = GetCounterpartFile () != null;
+		}
+
+		string GetCounterpartFile ()
+		{
+			var name = TryGetFilename ();
+			if (name == null) {
+				return null;
+			}
+
+			switch (Path.GetExtension (name.ToLower ())) {
+			case ".targets":
+				name = Path.ChangeExtension (name, ".props");
+				break;
+			case ".props":
+				name = Path.ChangeExtension (name, ".targets");
+				break;
+			default:
+				return null;
+			}
+
+			return File.Exists (name) ? name : null;
+		}
+	}
+}
--- MonoDevelop.MSBuildEditor/MSBuildCommands.cs
+++ MonoDevelop.MSBuildEditor/MSBuildCommands.cs
@@ -1,11 +1,8 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
 using MonoDevelop.Components.Commands;
 using MonoDevelop.Core;
-using MonoDevelop.Ide;
-using MonoDevelop.Refactoring;
 
 namespace MonoDevelop.MSBuildEditor
 {
