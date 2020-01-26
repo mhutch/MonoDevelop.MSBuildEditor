765dfc5adde17e4bcb7a6941d026688b87c57f4e
William Kent <wjk@users.noreply.github.com>
2020-01-26 13:37:18 -0500

    Add MSBuildContentTypeProvider
    This class adds MSBuild Editor support to all *.*proj files,
    without needing to register each and every type of project
    individually in MSBuildContentType.cs.

---
+++ MonoDevelop.MSBuild.Editor/MSBuildContentTypeProvider.cs
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.ComponentModel.Composition;
+using System.IO;
+using Microsoft.VisualStudio.Utilities;
+
+namespace MonoDevelop.MSBuild.Editor
+{
+	[Export(typeof(IFilePathToContentTypeProvider))]
+	[Name("MSBuild Content Type Provider")]
+	[FileExtension("*")]
+	class MSBuildContentTypeProvider : IFilePathToContentTypeProvider
+	{
+		[Import]
+		private IContentTypeRegistryService ContentTypeRegistryService { get; set; }
+
+		public bool TryGetContentTypeForFilePath (string filePath, out IContentType contentType)
+		{
+			string extension = Path.GetExtension (filePath).ToLowerInvariant ();
+			if (extension.EndsWith("proj")) {
+				contentType = ContentTypeRegistryService.GetContentType (MSBuildContentType.Name);
+				return true;
+			} else {
+				contentType = null;
+				return false;
+			}
+		}
+	}
+}
