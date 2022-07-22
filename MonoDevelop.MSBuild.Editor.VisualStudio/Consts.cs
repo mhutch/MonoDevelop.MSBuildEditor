360da2fc7b8dae988cd5fd7ac240c466902e176b
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2022-07-22 01:06:00 -0400

    Tidy up the VS extension a bit

---
+++ MonoDevelop.MSBuild.Editor.VisualStudio/Consts.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace MonoDevelop.MSBuild.Editor.VisualStudio
+{
+	class Consts
+	{
+		public const string PackageGuid = "6c7bd60d-5321-4fb0-8684-9736003d64ad";
+
+		public const string PackageName = "MSBuild Editor";
+		public const string PackageDescription = "Language service for MSBuild projects and targets";
+		public const string PackageVersion = ThisAssembly.AssemblyInformationalVersion;
+
+		public const string LanguageServiceGuid = "111e2ecb-9e5f-4945-9d21-d4e5368d620b";
+
+		public const string LanguageServiceName = "MSBuild";
+		public const string LanguageServiceKey = @"Languages\Language Services\" + LanguageServiceName;
+	}
+}
--- MonoDevelop.MSBuild.Editor.VisualStudio/MSBuildEditorVisualStudioPackage.cs
+++ MonoDevelop.MSBuild.Editor.VisualStudio/MSBuildEditorVisualStudioPackage.cs
@@ -11,36 +11,41 @@ using Task = System.Threading.Tasks.Task;
 
 namespace MonoDevelop.MSBuild.Editor.VisualStudio
 {
+
+	[InstalledProductRegistration(Consts.PackageName, Consts.PackageDescription, Consts.PackageVersion)]
 	[PackageRegistration (UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-	[Guid (PackageGuidString)]
+	[Guid (Consts.PackageGuid)]
+
 	[ProvideLanguageService (
 		typeof (MSBuildLanguageService),
-		LanguageServiceName, 101,
+		Consts.LanguageServiceName, 101,
 		ShowDropDownOptions = false,
 		RequestStockColors = true,
 		DefaultToInsertSpaces = true,
 		ShowSmartIndent = true
 		)]
+
 	// dunno why this isn't part of ProvideLanguageService 🤷‍
-	[SetRegistrationOption (LanguageServiceKey, "ShowBraceCompletion", 1)]
-	[ProvideLanguageExtension (typeof (MSBuildLanguageService), ".targets")]
-	[ProvideLanguageExtension (typeof (MSBuildLanguageService), ".props")]
-	[ProvideLanguageExtension (typeof (MSBuildLanguageService), ".tasks")]
-	[ProvideLanguageExtension (typeof (MSBuildLanguageService), ".overridetasks")]
-	[ProvideLanguageExtension (typeof (MSBuildLanguageService), ".csproj")]
-	[ProvideLanguageExtension (typeof (MSBuildLanguageService), ".vbproj")]
-	[ProvideLanguageExtension (typeof (MSBuildLanguageService), ".fsproj")]
-	[ProvideLanguageExtension (typeof (MSBuildLanguageService), ".xproj")]
-	[ProvideLanguageExtension (typeof (MSBuildLanguageService), ".vcxproj")]
-	[ProvideLanguageExtension (typeof (MSBuildLanguageService), ".proj")]
-	[ProvideLanguageExtension (typeof (MSBuildLanguageService), ".user")]
+	[SetRegistrationOption (Consts.LanguageServiceKey, "ShowBraceCompletion", 1)]
+
+	[ProvideLanguageExtension (typeof (MSBuildLanguageService), MSBuildFileExtension.targets)]
+	[ProvideLanguageExtension (typeof (MSBuildLanguageService), MSBuildFileExtension.props)]
+	[ProvideLanguageExtension (typeof (MSBuildLanguageService), MSBuildFileExtension.tasks)]
+	[ProvideLanguageExtension (typeof (MSBuildLanguageService), MSBuildFileExtension.overridetasks)]
+	[ProvideLanguageExtension (typeof (MSBuildLanguageService), MSBuildFileExtension.csproj)]
+	[ProvideLanguageExtension (typeof (MSBuildLanguageService), MSBuildFileExtension.vbproj)]
+	[ProvideLanguageExtension (typeof (MSBuildLanguageService), MSBuildFileExtension.fsproj)]
+	[ProvideLanguageExtension (typeof (MSBuildLanguageService), MSBuildFileExtension.xproj)]
+	[ProvideLanguageExtension (typeof (MSBuildLanguageService), MSBuildFileExtension.vcxproj)]
+	[ProvideLanguageExtension (typeof (MSBuildLanguageService), MSBuildFileExtension.proj)]
+	[ProvideLanguageExtension (typeof (MSBuildLanguageService), MSBuildFileExtension.user)]
+
 	public sealed class MSBuildEditorVisualStudioPackage : AsyncPackage
 	{
-		public const string PackageGuidString = "6c7bd60d-5321-4fb0-8684-9736003d64ad";
-		public const string LanguageServiceName = MSBuildContentType.Name;
-		public const string LanguageServiceKey = @"Languages\Language Services\" + LanguageServiceName;
 
 		protected override Task InitializeAsync (CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
-			=> Task.CompletedTask;
+		{
+			return base.InitializeAsync (cancellationToken, progress);
+		}
 	}
 }
