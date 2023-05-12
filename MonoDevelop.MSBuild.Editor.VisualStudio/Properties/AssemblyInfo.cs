e2967497ae74ec1770f6245043a4b80c1236fe3c
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2023-05-11 22:52:37 -0400

    Fix VS extension package
    * Update package references
    * Switch to SDK-style project so it's easier to edit project file

--- MonoDevelop.MSBuild.Editor.VisualStudio/Properties/AssemblyInfo.cs
+++ MonoDevelop.MSBuild.Editor.VisualStudio/Properties/AssemblyInfo.cs
@@ -1,17 +1,4 @@
-using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle ("MonoDevelop.MSBuild.Editor.VisualStudio")]
-[assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("MonoDevelop.MSBuild.Editor.VisualStudio")]
-[assembly: AssemblyCopyright ("")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
