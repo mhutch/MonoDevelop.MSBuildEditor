1d2de5d491bb55b7f3fa60080fba4518529ab097
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2023-08-22 16:48:45 -0400

    Simplify MSBuildCompletionItemManager
    We can now subclass XmlCompletionItemManager, and no longer
    need to look it up by name to delegate to it.

--- MonoDevelop.MSBuild.Editor/Completion/MSBuildCompletionItemManager.cs
+++ MonoDevelop.MSBuild.Editor/Completion/MSBuildCompletionItemManager.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
+using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion;
 using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion.Data;
 using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.PatternMatching;
 using Microsoft.VisualStudio.Utilities;
 
+using MonoDevelop.Xml.Editor.Completion;
 using MonoDevelop.Xml.Editor.Logging;
 using MonoDevelop.Xml.Logging;
 
@@ -21,116 +24,57 @@ namespace MonoDevelop.MSBuild.Editor.Completion
 	[Name (nameof (MSBuildCompletionItemManagerProvider))]
 	[ContentType (MSBuildContentType.Name)]
 	[TextViewRole (PredefinedTextViewRoles.Editable)]
-	[Order (Before = "XmlCompletionItemManagerProvider")]
+	[Order (Before = XmlCompletionItemManager.ProviderName)]
 	[Order (Before = PredefinedCompletionNames.DefaultCompletionItemManager)]
 	internal sealed class MSBuildCompletionItemManagerProvider : IAsyncCompletionItemManagerProvider
 	{
-		IEnumerable<Lazy<IAsyncCompletionItemManagerProvider, IOrderableContentTypeAndOptionalTextViewRoleMetadata>> unorderedCompletionItemManagerProviders;
-
-		IList<Lazy<IAsyncCompletionItemManagerProvider, IOrderableContentTypeAndOptionalTextViewRoleMetadata>> _orderedCompletionItemManagerProviders;
-		IList<Lazy<IAsyncCompletionItemManagerProvider, IOrderableContentTypeAndOptionalTextViewRoleMetadata>> OrderedCompletionItemManagerProviders
-			=> _orderedCompletionItemManagerProviders ??= Orderer.Order (unorderedCompletionItemManagerProviders);
-
-		public IEditorLoggerFactory LoggerFactory { get; }
+		readonly IEditorLoggerFactory loggerFactory;
+		readonly IPatternMatcherFactory patternMatcherFactory;
 
 		[ImportingConstructor]
 		public MSBuildCompletionItemManagerProvider (
 			IEditorLoggerFactory loggerFactory,
-			[ImportMany] IEnumerable<Lazy<IAsyncCompletionItemManagerProvider, IOrderableContentTypeAndOptionalTextViewRoleMetadata>> unorderedCompletionItemManagerProviders)
+			IPatternMatcherFactory patternMatcherFactory)
 		{
-			this.LoggerFactory = loggerFactory;
-			this.unorderedCompletionItemManagerProviders = unorderedCompletionItemManagerProviders;
+			this.loggerFactory = loggerFactory;
+			this.patternMatcherFactory = patternMatcherFactory;
 		}
 
-		IAsyncCompletionItemManager IAsyncCompletionItemManagerProvider.GetOrCreate (ITextView textView)
-			=> LoggerFactory.GetLogger<MSBuildCompletionItemManagerProvider> (textView).InvokeAndLogExceptions (() => GetOrCreate (textView));
-
-		IAsyncCompletionItemManager GetOrCreate (ITextView textView)
-			=> textView.Properties.GetOrCreateSingletonProperty (typeof (MSBuildCompletionItemManagerProvider), () => {
-				// each content type can only have a single item manager. we don't want to replace the item manager as
-				// the existing one provides important functionality. instead, we find the next provider after this one
-				// that's valid for MSBuild, which should be the XML one, but should fall back to the default one
-				IAsyncCompletionItemManager nextManager = GetNextProvider (textView, textView.TextSnapshot.ContentType, textView.Roles);
-				return new MSBuildCompletionItemManager (nextManager, this);
-			});
-
-		IAsyncCompletionItemManager GetNextProvider (ITextView textView, IContentType contentType, ITextViewRoleSet roles)
+		public IAsyncCompletionItemManager GetOrCreate (ITextView textView)
 		{
-			bool foundThis = false;
-			var nextProvider = OrderedCompletionItemManagerProviders
-				.Where (p => {
-					if (!foundThis) {
-						if (p.Metadata.Name == nameof (MSBuildCompletionItemManagerProvider)) {
-							foundThis = true;
-						}
-						return false;
-					}
-					if (!p.Metadata.ContentTypes.Any (c => contentType.IsOfType (c))) {
-						return false;
-					}
-					if (p.Metadata.TextViewRoles != null && roles != null && !roles.ContainsAny (p.Metadata.TextViewRoles)) {
-						return false;
-					}
-					return true;
-				}).First ();
-			var nextManager = nextProvider.Value.GetOrCreate (textView);
-			return nextManager;
+			return textView.Properties.GetOrCreateSingletonProperty (() =>
+				new MSBuildCompletionItemManager (patternMatcherFactory, loggerFactory.CreateLogger<MSBuildCompletionItemManager> (textView))
+			);
 		}
 	}
 
-	interface IOrderableContentTypeAndOptionalTextViewRoleMetadata : IContentTypeMetadata, IOrderable
+	internal sealed class MSBuildCompletionItemManager : XmlCompletionItemManager
 	{
-		[DefaultValue (null)]
-		IEnumerable<string> TextViewRoles { get; }
-	}
-
-	internal sealed class MSBuildCompletionItemManager : IAsyncCompletionItemManager
-	{
-		readonly MSBuildCompletionItemManagerProvider provider;
-		readonly IAsyncCompletionItemManager nextManager;
-
-		public MSBuildCompletionItemManager (IAsyncCompletionItemManager nextManager, MSBuildCompletionItemManagerProvider provider)
+		public MSBuildCompletionItemManager (IPatternMatcherFactory patternMatcherFactory, ILogger logger) : base (patternMatcherFactory, logger)
 		{
-			this.nextManager = nextManager;
-			this.provider = provider;
 		}
 
-		public Task<ImmutableArray<CompletionItem>> SortCompletionListAsync (IAsyncCompletionSession session, AsyncCompletionSessionInitialDataSnapshot data, CancellationToken token)
+		protected override FilteredCompletionModel UpdateCompletionList (IAsyncCompletionSession session, AsyncCompletionSessionDataSnapshot data, CancellationToken token)
 		{
-			return nextManager.SortCompletionListAsync (session, data, token);
-		}
-
-		public Task<FilteredCompletionModel> UpdateCompletionListAsync (IAsyncCompletionSession session, AsyncCompletionSessionDataSnapshot data, CancellationToken token)
-			=> provider.LoggerFactory.GetLogger<MSBuildCompletionItemManager> (session.TextView).InvokeAndLogExceptions (() => UpdateCompletionListAsyncInternal (session, data, token));
-
-		public Task<FilteredCompletionModel> UpdateCompletionListAsyncInternal (IAsyncCompletionSession session, AsyncCompletionSessionDataSnapshot data, CancellationToken token)
-		{
-			Task<FilteredCompletionModel> Next () => nextManager.UpdateCompletionListAsync (session, data, token);
-
 			switch (data.Trigger.Reason) {
 			case CompletionTriggerReason.Deletion:
 			case CompletionTriggerReason.Insertion:
 			case CompletionTriggerReason.Backspace:
 			case CompletionTriggerReason.Invoke:
+				if (session.Properties.TryGetProperty (typeof (MSBuildCompletionSource.NuGetSearchUpdater), out MSBuildCompletionSource.NuGetSearchUpdater searchInfo)) {
+					//don't pass the CancellationToken to the search job, else filtering operations will cancel searches
+					var newList = searchInfo.Update (this, data);
+
+					if (newList.Length != data.InitialSortedList.Length) {
+						data = new AsyncCompletionSessionDataSnapshot (
+							newList, data.Snapshot, data.Trigger, data.InitialTrigger, data.SelectedFilters, data.IsSoftSelected, data.DisplaySuggestionItem
+						);
+					}
+				}
 				break;
-			default:
-				return Next ();
 			}
 
-			if (!session.Properties.TryGetProperty (typeof (MSBuildCompletionSource.NuGetSearchUpdater), out MSBuildCompletionSource.NuGetSearchUpdater searchInfo)) {
-				return Next ();
-			}
-
-			//don't pass the CancellationToken to the search job, else filtering operations will cancel searches
-			var newList = searchInfo.Update (this, data);
-
-			if (newList.Length != data.InitialSortedList.Length) {
-				data = new AsyncCompletionSessionDataSnapshot (
-					newList, data.Snapshot, data.Trigger, data.InitialTrigger, data.SelectedFilters, data.IsSoftSelected, data.DisplaySuggestionItem
-				);
-			}
-
-			return Next ();
+			return base.UpdateCompletionList (session, data, token);
 		}
 	}
 }
