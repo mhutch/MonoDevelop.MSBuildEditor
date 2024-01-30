7395e2d0dde0ed168b5a96a871ab700996313ad9
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2024-01-29 22:29:15 -0500

    Work around IVsPackageSourceProvider issue

--- MonoDevelop.MSBuild.Editor.VisualStudio/PackageFeedRegistryProvider.cs
+++ MonoDevelop.MSBuild.Editor.VisualStudio/PackageFeedRegistryProvider.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
+using System.Threading.Tasks;
 
 using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.Threading;
 using Microsoft.VisualStudio.Utilities;
 
 using MonoDevelop.MSBuild.Editor.Completion;
+using MonoDevelop.Xml.Logging;
 
 using NuGet.VisualStudio;
 
@@ -18,38 +21,61 @@ namespace MonoDevelop.MSBuild.Editor.VisualStudio
 	[Name ("Visual Studio Package Feed Registry Provider")]
 	partial class PackageFeedRegistryProvider : IPackageFeedRegistryProvider
 	{
+		readonly object gate = new ();
+		readonly JoinableTaskContext joinableTaskContext;
 		readonly IVsPackageSourceProvider provider;
 		readonly ILogger logger;
+		Task<IReadOnlyList<string>> packageSourcesTask;
 
 		[ImportingConstructor]
-		public PackageFeedRegistryProvider (IVsPackageSourceProvider provider, MSBuildEnvironmentLogger logger)
+		public PackageFeedRegistryProvider (JoinableTaskContext joinableTaskContext, IVsPackageSourceProvider provider, MSBuildEnvironmentLogger logger)
 		{
+			this.joinableTaskContext = joinableTaskContext;
 			this.provider = provider;
 			this.logger = logger.Logger;
+
+			provider.SourcesChanged += OnSourcesChanged;
+			OnSourcesChanged (this, EventArgs.Empty);
 		}
 
+		void OnSourcesChanged (object sender, EventArgs e)
+		{
+			lock(gate) {
+				packageSourcesTask = Task.Run (() => GetPackageSourcesAsync ()).WithTaskExceptionLogger (logger);
+			}
+		}
+
+		async Task<IReadOnlyList<string>> GetPackageSourcesAsync ()
+		{
+			// docs for IVsPackageSourceProvider are wrong, in VS 17.3 and later it's no longer thread safe
+			await joinableTaskContext.Factory.SwitchToMainThreadAsync ();
+
+			List<string> sources;
+			try {
+				 sources = provider.GetSources (true, false).Select (s => s.Value).ToList ();
+			} catch (Exception ex) {
+				LogFailedToGetConfiguredSources (logger, ex);
+				sources = [ "https://api.nuget.org/v3/index.json" ];
+			}
+
+			// make sure we always have the installed package cache as a source
+			if (!sources.Any (x => x.IndexOf ("\\.nuget", StringComparison.OrdinalIgnoreCase) > -1)) {
+				sources.Add (Environment.ExpandEnvironmentVariables ("%USERPROFILE%\\.nuget\\packages"));
+			}
+
+			return sources;
+		}
+
+		// TODO: on base interface, make this a "try" method and add the "changed" event
 		public IReadOnlyList<string> ConfiguredFeeds {
 			get {
-				var sources = new List<string> ();
-
-				// IVsPackageSourceProvider seems to be broken in 17.3 previews so add a fallback
-				try {
-					IEnumerable<KeyValuePair<string, string>> enabledSources = provider.GetSources (true, false);
-
-					foreach (KeyValuePair<string, string> curEnabledSource in enabledSources) {
-						string source = curEnabledSource.Value;
-						sources.Add (source);
-					}
-				} catch (Exception ex) {
-					LogFailedToGetConfiguredSources (logger, ex);
-					sources.Add ("https://api.nuget.org/v3/index.json");
+				if (packageSourcesTask is not null && packageSourcesTask.Status == TaskStatus.RanToCompletion) {
+// https://github.com/microsoft/vs-threading/issues/301
+#pragma warning disable VSTHRD002 // Avoid problematic synchronous waits
+					return packageSourcesTask.Result;
+#pragma warning restore VSTHRD002 // Avoid problematic synchronous waits
 				}
-
-				if (!sources.Any (x => x.IndexOf ("\\.nuget", StringComparison.OrdinalIgnoreCase) > -1)) {
-					sources.Add (Environment.ExpandEnvironmentVariables ("%USERPROFILE%\\.nuget\\packages"));
-				}
-
-				return sources;
+				return Array.Empty<string> ();
 			}
 		}
 
